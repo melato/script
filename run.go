f01e0be96478076d2193c9d5734f17da5980868a
Alex Athanasopoulos <alex@melato.org>
2023-04-05 20:43:03 +0300

    do not set first Cmd Stdin to os.Stdin. wrap Wait() error to include the executable path

--- run.go
+++ run.go
@@ -7,11 +7,11 @@ import (
 	"os/exec"
 )
 
-/** Run 0 or more commands.  If there are more than 1 command, pipe Stdout of each command to Stdin of the next command.
-  Any unset Stderr is set to os.Stderr
-  Stdin of the first command is set to os.Stdin, unless it is already set
-  Stdout of the last command is set to os.Stdout, unless it is already set
-  If the global Trace is true, print the commands before running them.
+/*
+  - Run 0 or more commands.  If there are more than 1 command, pipe Stdout of each command to Stdin of the next command.
+    Any unset Stderr is set to os.Stderr
+    Stdout of the last command is set to os.Stdout, unless it is already set
+    If the global Trace is true, print the commands before running them.
 */
 func Run(cmd ...*exec.Cmd) error {
 	n := len(cmd)
@@ -19,11 +19,8 @@ func Run(cmd ...*exec.Cmd) error {
 		return nil
 	}
 	var closers []io.Closer
-	first := cmd[0]
-	if first.Stdin == nil {
-		first.Stdin = os.Stdin
-	}
 	last := cmd[n-1]
+	//fmt.Printf("stdin=%T stdout=%T stderr=%T\n", last.Stdin, last.Stdout, last.Stderr)
 	if last.Stdout == nil {
 		last.Stdout = os.Stdout
 	}
@@ -73,6 +70,7 @@ func Run(cmd ...*exec.Cmd) error {
 	for i, c := range cmd {
 		err = c.Wait()
 		if err != nil {
+			err = fmt.Errorf("%s: %w", c.Path, err)
 			break
 		}
 		waited = i
